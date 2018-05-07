c5efec4b890d96a3a468d5e564327b1e6cec6d0b
Your Name <you@example.com>
2018-05-07 13:21:24 +0300

    Added background-image,background-repeat,text-align,Padding to.header

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+    //business interface logic
+    Var number;
+
+    function condition (){
+       for (var count =1; count <=number;count ++){
+          if (count % 3 || 5===0) {
+            $("#result").append("<li>") + "PingPong"  + "</li>)";
+          } else if (count %3===0){
+            $("#result").append("<li>") + "ping" + "</li>)";
+          } else if (count % 5===0){
+            $("#result").append("<li") + "pong" + "</li>)";
+          } else {
+            $("#result").append("<li>") + "count.toString() + "(</li>");
+          }
+        }
+     }
+
+//user interface logic
+  $(document).ready(function(){
+   $("form#divisibleNumber").submit(function(event){
+      number =parseInt($("#no").val());
+       $("#result").text("");
+         condition();
+         $("#result").show();
+         $("#no").val('');
+        event.preventDefault();
+      });
+    });
