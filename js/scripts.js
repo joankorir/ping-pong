6eaf0a2924d6cea0a4757f08e9139c0678d19926
Your Name <you@example.com>
2018-05-07 18:29:41 +0300

    Added centered the text under class header

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
     //business interface logic
-    Var number;
-
-    function condition (){
+    Var number=function condition (){
        for (var count =1; count <=number;count ++){
           if (count % 3 || 5===0) {
             $("#result").append("<li>") + "PingPong"  + "</li>)";
@@ -10,19 +8,21 @@
           } else if (count % 5===0){
             $("#result").append("<li") + "pong" + "</li>)";
           } else {
-            $("#result").append("<li>") + "count.toString() + "(</li>");
-          }
+            $("#result").append("<li>") + count.toString() + "</li>");          }
         }
      }
+   }
 
 //user interface logic
   $(document).ready(function(){
-   $("form#divisibleNumber").submit(function(event){
-       number =parseInt($("#no").val());
-       ("#result").text("result");
-         condition();
-         $("#result").show();
-         $("#no").val('');
-        event.preventDefault();
+    $("form#divisibleNumber").submit(function(event){
+      var number = parseInt($("#no").val());
+      $("#result").text("");
+       condition();
+       $("#result").show();
+       $("#no").val('');
+       event.preventDefault();
+
+
       });
     });
