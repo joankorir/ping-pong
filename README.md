cb546d93a213229b38727aaad454d56dfddbed50
Your Name <you@example.com>
2018-05-08 11:12:15 +0300

    inserted font style under class panel-

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
- # ping-pong
+  # ping-pong
 #### {The project is about a website of a game known as Ping-pong}, {7th april 2018}
 #### By Joan korir
 ## Description
@@ -10,4 +10,5 @@
  I used much of javascript i played along with the user and business logic interface.Dealing with variables.
 ## Support and contact details
 When u come across to any problem do Email me joankorir44@gmail.com
+
 ##link to live site on GitHub Pages{ https://joankorir.github.io/ping-pong/}
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
     //business interface logic
-    Var number=function condition (){
+    Var number;
+      function condition (){
        for (var count =1; count <=number;count ++){
-          if (count % 3 || 5===0) {
-            $("#result").append("<li>") + "PingPong"  + "</li>)";
+          if (count % 15===0) {
+            $("#result").append("<li>") + "ping-pong"  + "</li>)";
           } else if (count %3===0){
-            $("#result").append("<li>") + "ping" + "</li>)";
+            $("#result").append("<li>") + "ping" + "</li>");
           } else if (count % 5===0){
-            $("#result").append("<li") + "pong" + "</li>)";
+            $("#result").append("<li") + "pong" + "</li>");
           } else {
             $("#result").append("<li>") + count.toString() + "</li>");          }
         }
@@ -16,7 +17,7 @@
 //user interface logic
   $(document).ready(function(){
     $("form#divisibleNumber").submit(function(event){
-      var number = parseInt($("#no").val());
+      number = parseInt($("#no").val());
       $("#result").text("");
        condition();
        $("#result").show();
